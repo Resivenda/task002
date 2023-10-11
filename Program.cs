c957eeab0e87639a410ff99dbeaaccb7f5e027f3
Alenae <alena_trsenkov@list.ru>
2023-10-12 01:16:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру (с конца) заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100)
+{
+    int ThirdDigit = ((number /100)%10);
+
+    Console.WriteLine("ThirdDigit=" + ThirdDigit);
+
+}
+else Console.WriteLine("Число должно быть трехзначным и более");
